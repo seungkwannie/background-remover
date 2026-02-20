8bb4ca7428d5f696490132d61b077de536cd0540
myreguiang <aveguiang123@gmail.com>
2026-02-20 21:50:05 +0800

    "reworked files!"

--- docs/app.js
+++ docs/app.js
@@ -13,7 +13,7 @@ removeBtn.addEventListener("click", async () => {
   const formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("https://background-remover-hcj4.onrender.com/remove-bg", {
+  const response = await fetch("https://background-remover-hcj4.onrender.com/bg-remover", {
     method: "POST",
     body: formData,
   });
